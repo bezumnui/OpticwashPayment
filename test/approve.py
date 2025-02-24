95503e3e085747ba49173bcc9df2e0e33ede058c
bezikmac <bezumnui.mistikgt@gmail.com>
2025-02-24 15:41:05 +0400

    test update

--- test/approve.py
+++ test/approve.py
@@ -7,8 +7,10 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     opticwash = PyOpticwash()
     opticwash.start()
+    input("Press enter to approve\n")
     opticwash.approve_transaction()
     input("Press enter to stop listening\n")
+
 
     # opticwash.open_cabinet()
     opticwash.stop()
