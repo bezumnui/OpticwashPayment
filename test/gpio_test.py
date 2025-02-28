63c5fb78f3014e81bb5286a1388a0044c21673d9
bezikmac <bezumnui.mistikgt@gmail.com>
2025-02-28 17:22:36 +0400

    gpio test

--- test/gpio_test.py
+++ test/gpio_test.py
@@ -1,3 +1,5 @@
+import time
+
 from gpiozero import OutputDevice
 
 display_controller_hid = OutputDevice(17)
@@ -6,5 +8,5 @@ if __name__ == '__main__':
     while True:
         input("Press enter to on\n")
         display_controller_hid.on()
-        input("Press enter to off\n")
+        time.sleep(.2);
         display_controller_hid.off()
