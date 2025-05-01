4d5f74b81bd9ae3d7ca406e925405767a5986aab
bezikmac <bezumnui.mistikgt@gmail.com>
2025-05-01 19:28:21 +0400

    test

---
+++ test/pol.py
@@ -0,0 +1,17 @@
+import logging
+import time
+
+from pyOpticwash.client import PyOpticwash
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    opticwash = PyOpticwash()
+    # opticwash.raw_mdb.start_polling()
+    opticwash.raw_mdb.mdb.send_raw("R,10")
+    opticwash.raw_mdb.mdb.send_raw("R,11,0003000000")
+    opticwash.raw_mdb.mdb.send_raw("R,14,01")
+    while True:
+        opticwash.raw_mdb.mdb.send_raw("R,12")
+        time.sleep(0.1)
+        # opticwash.raw_mdb.mdb.send_raw(input().encode())
+        # input("Press enter...")
