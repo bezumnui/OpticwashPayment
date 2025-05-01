32eedb847b9570b1ee7c5ea2c80a4253357479c6
bezikmac <bezumnui.mistikgt@gmail.com>
2025-05-01 18:36:49 +0400

    test

--- test/pooling.py
+++ test/pooling.py
@@ -5,7 +5,9 @@ from pyOpticwash.client import PyOpticwash
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     opticwash = PyOpticwash()
-    # opticwash.raw_mdb.start_polling()
+    # opticwash.raw_mdb.request_vending()
+    opticwash.raw_mdb.start_polling()
+    opticwash.raw_mdb.request_vending(10)
     while True:
         print(opticwash.raw_mdb.mdb.send_raw_message_with_response(input().encode()))
         input("Press enter...")
