ccc393fca36c862935650255c7a6692dd488b413
bezikmac <bezumnui.mistikgt@gmail.com>
2025-05-01 17:27:13 +0400

    test

--- test/pooling.py
+++ test/pooling.py
@@ -1,6 +1,9 @@
+import logging
+
 from pyOpticwash.client import PyOpticwash
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     opticwash = PyOpticwash()
     opticwash.start()
     opticwash.mdb_client.send_raw("R,18")
