ba5b72a8c4be83b28b5c8fc8f4da286b91040a35
bezikmac <bezumnui.mistikgt@gmail.com>
2025-05-01 17:58:47 +0400

    test

--- test/pooling.py
+++ test/pooling.py
@@ -5,7 +5,7 @@ from pyOpticwash.client import PyOpticwash
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     opticwash = PyOpticwash()
-    opticwash.start()
+    opticwash.raw_mdb.start()
     while True:
-        opticwash.mdb_client.send_raw(b"R,18")
+        opticwash.raw_mdb.send_raw(b"R,18")
         input()
