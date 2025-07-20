b32a991b0de1e6755cd669f3a8942134942e32bb
bezikmac <bezumnui.mistikgt@gmail.com>
2025-07-20 21:35:10 +0400

    skibidi dop dop

--- rp2040/src_hid/build/CMakeFiles/dev_hid_composite.dir/Users/bezikmac/Documents/Arduino/Pico/test/pico-sdk/lib/tinyusb/src/class/msc/msc_device.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/dev_hid_composite.dir/Users/bezikmac/Documents/Arduino/Pico/test/pico-sdk/lib/tinyusb/src/class/msc/msc_device.c.o: \
- /Users/bezikmac/Documents/Arduino/Pico/test/pico-sdk/lib/tinyusb/src/class/msc/msc_device.c \
- /Users/bezikmac/Documents/Arduino/Pico/test/pico-sdk/lib/tinyusb/src/tusb_option.h \
- /Users/bezikmac/Documents/Arduino/Pico/test/pico-sdk/lib/tinyusb/src/common/tusb_compiler.h \
- /Users/bezikmac/Documents/Arduino/Pico/test/src_hid/tusb_config.h \
- /Users/bezikmac/Documents/Arduino/Pico/test/pico-sdk/lib/tinyusb/src/common/tusb_mcu.h
